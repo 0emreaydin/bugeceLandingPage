5c83efba3b82d01952a486ca27ba61e925f80005
Emre Aydin <emre.aydn.1996@gmail.com>
2018-07-09 20:12:49 +0300

    initial

---
+++ src/components/Main.js
@@ -0,0 +1,161 @@
+import React from 'react';
+
+export const Main = () => (
+    <div>
+        <div id="section1" class="container-fluid">
+            <div class="row">
+                <div class="col-md-6" id="divLeft">
+                    <div class="center-element"> 
+                        <h1>ŞEHRİN EN İYİ ETKİNLİKLERİ PARMAĞININ UCUNDA</h1>
+                        <br/>
+                        <button type="button" class="btn btn-outline-dark">Hemen indir</button>
+                    </div>
+                </div>
+                <div class="col-md-6" id="divRight"></div>
+            </div>
+        </div>
+        <div id="section2" class="container-fluid">
+            <h1>BUGECE uygulamasına Play Store'dan erişebilirsiniz.</h1>
+            <div id="store">
+                <div class="row">
+                    <div class="col-md-12">
+                        <a href="https://play.google.com/store/apps/details?id=com.app.bugece&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"><img alt="Google Play'den alın" width="100%" src="https://play.google.com/intl/en_us/badges/images/generic/tr_badge_web_generic.png"/></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="section41" class="container-fluid">
+            <h1>Gece Hayatı Rehberi</h1>
+            <div id="content">
+                <div class="row" id="content" width="80%">
+                    <div class="col-md-12">
+                        <div class="row" id="content">
+                            <div class="col-md-4">
+                                <img alt="Gece Hayatı Rehberi" height="100%" src={require('../images/1.png')} />
+                            </div>
+                            <div class="col-md-8" id="info">
+                                <div class="row">
+                                    <div class="col-md-12" align="left">
+                                        <img alt="Gece Hayatı Rehberi" height="20%" src={require('../images/icons8-news-filled-100.png')} />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12" style={{paddingTop:'30px'}}>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non nisi est. Lobortis feugiat vivamus at augue. Urna id volutpat lacus laoreet non curabitur gravida arcu. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam. Neque vitae tempus quam pellentesque nec nam aliquam. Tellus mauris a diam maecenas sed enim ut. Egestas tellus rutrum tellus pellentesque eu. Proin libero nunc consequat interdum varius sit. Felis donec et odio pellentesque diam volutpat commodo. Nisl pretium fusce id velit ut tortor pretium viverra. Eget gravida cum sociis natoque penatibus. Amet nisl suscipit adipiscing bibendum est ultricies integer. Ultrices tincidunt arcu non sodales neque. Cras tincidunt lobortis feugiat vivamus at augue eget.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="section42" class="container-fluid">
+            <h1>Sana Özel Takvim</h1>
+            <div id="content">
+                <div class="row" id="content" width="80%">
+                    <div class="col-md-12">
+                        <div class="row" id="content">
+                            <div class="col-md-4">
+                                <img alt="Gece Hayatı Rehberi" height="100%" src={require('../images/2.png')} />
+                            </div>
+                            <div class="col-md-8" id="info">
+                                <div class="row">
+                                    <div class="col-md-12" align="left">
+                                        <img alt="Gece Hayatı Rehberi" height="20%" src={require('../images/icons8-calendar-filled-100.png')} />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12" style={{paddingTop:'30px'}}>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non nisi est. Lobortis feugiat vivamus at augue. Urna id volutpat lacus laoreet non curabitur gravida arcu. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam. Neque vitae tempus quam pellentesque nec nam aliquam. Tellus mauris a diam maecenas sed enim ut. Egestas tellus rutrum tellus pellentesque eu. Proin libero nunc consequat interdum varius sit. Felis donec et odio pellentesque diam volutpat commodo. Nisl pretium fusce id velit ut tortor pretium viverra. Eget gravida cum sociis natoque penatibus. Amet nisl suscipit adipiscing bibendum est ultricies integer. Ultrices tincidunt arcu non sodales neque. Cras tincidunt lobortis feugiat vivamus at augue eget.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="section43" class="container-fluid">
+            <h1>Biletlerini 9 saniyede al</h1>
+            <div id="content">
+                <div class="row" id="content" width="80%">
+                    <div class="col-md-12">
+                        <div class="row" id="content">
+                            <div class="col-md-4">
+                                <img alt="Gece Hayatı Rehberi" height="100%" src={require('../images/3.png')} />
+                            </div>
+                            <div class="col-md-8" id="info">
+                                <div class="row">
+                                    <div class="col-md-12" align="left">
+                                        <img alt="Gece Hayatı Rehberi" height="20%" src={require('../images/icons8-watch-filled-100 (1).png')} />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12" style={{paddingTop:'30px'}}>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non nisi est. Lobortis feugiat vivamus at augue. Urna id volutpat lacus laoreet non curabitur gravida arcu. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam. Neque vitae tempus quam pellentesque nec nam aliquam. Tellus mauris a diam maecenas sed enim ut. Egestas tellus rutrum tellus pellentesque eu. Proin libero nunc consequat interdum varius sit. Felis donec et odio pellentesque diam volutpat commodo. Nisl pretium fusce id velit ut tortor pretium viverra. Eget gravida cum sociis natoque penatibus. Amet nisl suscipit adipiscing bibendum est ultricies integer. Ultrices tincidunt arcu non sodales neque. Cras tincidunt lobortis feugiat vivamus at augue eget.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="section44" class="container-fluid">
+            <h1>Şehrin En iyi Etkinlikleri</h1>
+            <div id="content">
+                <div class="row" id="content" width="80%">
+                    <div class="col-md-12">
+                        <div class="row" id="content">
+                            <div class="col-md-4">
+                                <img alt="Gece Hayatı Rehberi" height="100%" src={require('../images/4.png')} />
+                            </div>
+                            <div class="col-md-8" id="info">
+                                <div class="row">
+                                    <div class="col-md-12" align="left">
+                                        <img alt="Gece Hayatı Rehberi" height="20%" src={require('../images/icons8-marker-filled-100.png')} />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12" style={{paddingTop:'30px'}}>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non nisi est. Lobortis feugiat vivamus at augue. Urna id volutpat lacus laoreet non curabitur gravida arcu. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam. Neque vitae tempus quam pellentesque nec nam aliquam. Tellus mauris a diam maecenas sed enim ut. Egestas tellus rutrum tellus pellentesque eu. Proin libero nunc consequat interdum varius sit. Felis donec et odio pellentesque diam volutpat commodo. Nisl pretium fusce id velit ut tortor pretium viverra. Eget gravida cum sociis natoque penatibus. Amet nisl suscipit adipiscing bibendum est ultricies integer. Ultrices tincidunt arcu non sodales neque. Cras tincidunt lobortis feugiat vivamus at augue eget.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="section45" class="container-fluid">
+            <h1>Okut ve Geç</h1>
+            <div id="content">
+                <div class="row" id="content" width="80%">
+                    <div class="col-md-12">
+                        <div class="row" id="content">
+                            <div class="col-md-4">
+                                <img alt="Gece Hayatı Rehberi" height="100%" src={require('../images/5.png')} />
+                            </div>
+                            <div class="col-md-8" id="info">
+                                <div class="row">
+                                    <div class="col-md-12" align="left">
+                                        <img alt="Gece Hayatı Rehberi" height="20%" src={require('../images/icons8-ticket-filled-100 (1).png')} />
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12" style={{paddingTop:'30px'}}>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non nisi est. Lobortis feugiat vivamus at augue. Urna id volutpat lacus laoreet non curabitur gravida arcu. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam. Neque vitae tempus quam pellentesque nec nam aliquam. Tellus mauris a diam maecenas sed enim ut. Egestas tellus rutrum tellus pellentesque eu. Proin libero nunc consequat interdum varius sit. Felis donec et odio pellentesque diam volutpat commodo. Nisl pretium fusce id velit ut tortor pretium viverra. Eget gravida cum sociis natoque penatibus. Amet nisl suscipit adipiscing bibendum est ultricies integer. Ultrices tincidunt arcu non sodales neque. Cras tincidunt lobortis feugiat vivamus at augue eget.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="section3" class="container-fluid">
+            <h1>Bizi sosyal medyadan takip edebilirsiniz.</h1>
+        </div>
+    </div>
+);
